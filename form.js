a799ea6d54f1a8a8e72d0e88c23660672e1ae2c0
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-10-09 12:00:54 -0400

    Create form.js

---
+++ form.js
@@ -0,0 +1,20 @@
+function form(){
+return (
+  `
+<form action="/" method="post">
+  <label for="fname">Title</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Description</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="pname">Price</label>
+  <input type="number" id="pname" name="pname"><br><br>
+  <label for="imgname">Images</label>
+  <input type="file" id="imgname" name="imgname" accept="image/png, image/jpeg" />
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
+}
+`)
+}
+
+export default function form
